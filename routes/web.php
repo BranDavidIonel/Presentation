e362a30368b7c8e203ac97fa1b23f029f6fe5904
Bran David <david.bran@gosocialdev.eu>
2023-01-02 14:25:57 +0200

    Revert "comment is admin middleware from routes"
    This reverts commit 18df2bd00d53cbd44ab4bbc26f21ef717ef75c6d.

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,7 @@ Route::get('/', [ProjectController::class, 'index'])->name('top-projects');
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-// old "is.admin"
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth','is.admin']], function () {
     Route::get('/projects/create', [ProjectController::class, 'showCreate'])->name('projects.show-create');
     Route::get('/projects/list', [ProjectController::class, 'listAll'])->name('projects.list');
 });
