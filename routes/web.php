18df2bd00d53cbd44ab4bbc26f21ef717ef75c6d
Bran David <david.bran@gosocialdev.eu>
2023-01-02 14:22:27 +0200

    comment is admin middleware from routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ Route::get('/', [ProjectController::class, 'index'])->name('top-projects');
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::group(['middleware' => ['auth','is.admin']], function () {
+// old "is.admin"
+Route::group(['middleware' => ['auth']], function () {
     Route::get('/projects/create', [ProjectController::class, 'showCreate'])->name('projects.show-create');
     Route::get('/projects/list', [ProjectController::class, 'listAll'])->name('projects.list');
 });
