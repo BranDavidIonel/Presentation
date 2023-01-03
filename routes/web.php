6962506b50e2055dae1c50a629d10b7d321b846a
Bran David <david.bran@gosocialdev.eu>
2023-01-03 19:33:37 +0200

    add prefix 'projects' in routes web

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@ Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 //Route::group(['middleware' => ['auth','is.admin']], function () {
-    Route::get('/projects/create', [ProjectController::class, 'showCreate'])->name('projects.show-create');
-    Route::get('/projects/{project}/edit', [ProjectController::class, 'showEdit'])->name('projects.show-edit');
-    Route::get('/projects/list', [ProjectController::class, 'listAll'])->name('projects.list');
-    Route::post('/projects/store', [ProjectController::class, 'store'])->name('projects.store');
-    Route::put('/projects/{project}/update', [ProjectController::class, 'update'])->name('projects.update');
-    Route::delete('/projects/{project}/delete', [ProjectController::class, 'delete'])->name('projects.delete');
+Route::prefix('projects')->group(function () {
+    Route::get('/create', [ProjectController::class, 'showCreate'])->name('projects.show-create');
+    Route::get('{project}/edit', [ProjectController::class, 'showEdit'])->name('projects.show-edit');
+    Route::get('/list', [ProjectController::class, 'listAll'])->name('projects.list');
+    Route::post('store', [ProjectController::class, 'store'])->name('projects.store');
+    Route::put('{project}/update', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('{project}/delete', [ProjectController::class, 'delete'])->name('projects.delete');
+});
 //});
