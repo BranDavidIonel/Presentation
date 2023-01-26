dd0c234b05760c13bd9f8004c9893279a7b4e17b
david <bran.david@vlinde.com>
2023-01-26 14:36:41 +0200

    put back in middleware admin routes

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ Route::get('/', [ProjectController::class, 'index'])->name('top-projects');
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-//Route::group(['middleware' => ['auth','is.admin']], function () {
-Route::prefix('projects')->group(function () {
-    Route::get('/create', [ProjectController::class, 'showCreate'])->name('projects.show-create');
-    Route::get('{project}/edit', [ProjectController::class, 'showEdit'])->name('projects.show-edit');
-    Route::get('/list', [ProjectController::class, 'listAll'])->name('projects.list');
-    Route::post('store', [ProjectController::class, 'store'])->name('projects.store');
-    Route::put('{project}/update', [ProjectController::class, 'update'])->name('projects.update');
-    Route::delete('{project}/delete', [ProjectController::class, 'delete'])->name('projects.delete');
+Route::group(['middleware' => ['auth', 'is.admin']], function () {
+    Route::prefix('projects')->group(function () {
+        Route::get('/create', [ProjectController::class, 'showCreate'])->name('projects.show-create');
+        Route::get('{project}/edit', [ProjectController::class, 'showEdit'])->name('projects.show-edit');
+        Route::get('/list', [ProjectController::class, 'listAll'])->name('projects.list');
+        Route::post('store', [ProjectController::class, 'store'])->name('projects.store');
+        Route::put('{project}/update', [ProjectController::class, 'update'])->name('projects.update');
+        Route::delete('{project}/delete', [ProjectController::class, 'delete'])->name('projects.delete');
+    });
 });
-//});
